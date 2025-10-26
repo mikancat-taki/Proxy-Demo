ef7e52855b5898d22dff897d91229e665c563585
mikancat10 <216382990+mikancat-taki@users.noreply.github.com>
2025-10-26 14:23:17 +0900

    Delete src/サーバーでログを使う.ts

--- src/サーバーでログを使う.ts
+++
@@ -1,9 +0,0 @@
-import logger from "./logger";
-app.use((req, res, next) => {
-  const start = Date.now();
-  res.on("finish", () => {
-    const duration = Date.now() - start;
-    logger.info({ method: req.method, path: req.path, status: res.statusCode, duration }, "http");
-  });
-  next();
-});
