288054f9e15f1ca80243e688e9457934a67a6ef5
mikancat10 <216382990+mikancat-taki@users.noreply.github.com>
2025-10-26 13:27:32 +0900

    Create security.ts

---
+++ middleware/security.ts
@@ -0,0 +1,8 @@
+import { Request, Response, NextFunction } from "express";
+
+export default function security(req: Request, res: Response, next: NextFunction) {
+  res.setHeader("X-Content-Type-Options", "nosniff");
+  res.setHeader("X-Frame-Options", "DENY");
+  res.setHeader("Referrer-Policy", "no-referrer");
+  next();
+}
