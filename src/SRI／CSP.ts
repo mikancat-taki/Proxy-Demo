1ec762805ab011f30e59786254b8f32ba0095b37
mikancat10 <216382990+mikancat-taki@users.noreply.github.com>
2025-10-26 14:22:20 +0900

    Delete src/SRI／CSP.ts

--- src/SRI／CSP.ts
+++
@@ -1,9 +0,0 @@
-// Remove or override CSP header to allow proxy usage
-res.removeHeader("Content-Security-Policy");
-res.setHeader("Content-Security-Policy", "default-src * 'unsafe-inline' 'unsafe-eval' data: blob:;");
-
-// In HTML rewrite: remove integrity attributes
-document.querySelectorAll("script[integrity], link[integrity]").forEach(el => {
-  el.removeAttribute("integrity");
-  el.removeAttribute("crossorigin");
-});
